9cfa95f9c0af19548dd11612b65e67b4098c71c0
Carolina Vallejo <carovallejomar@gmail.com>
2017-12-28 23:12:26 +0100

    prism

--- code.js
+++ code.js
@@ -21,9 +21,9 @@ class Gistbox extends React.Component {
       value : 'java',
       queries: Array(30).fill(''),
       raw: Array(30).fill(''),
-      opencode: '',
       inputCliked : false,
-      actualIndexBox: 0
+      actualIndexBox: 0,
+      actualTitleBox: ''
     };
   }
 
@@ -33,8 +33,12 @@ class Gistbox extends React.Component {
       .then(response => response.json())
       .then(data => {
 
+
+
         console.log(data);
         this.setState({data: data });
+        console.log();
+
     })
       .catch(err => console.error(this.url, err.toString()))
   }
@@ -55,18 +59,29 @@ class Gistbox extends React.Component {
   componentDidMount() {
     this.loadData(this.state.query);
   }
+  componentDidUpdate(){
+    if(typeof this.codeTag !== 'undefined'){
+     Prism.highlightElement(this.codeTag, Prism.languages.javascript);
+    }
+
+
+    
+  }
 
   handleClick(i, event) {
 
-    this.setState({opencode:'open'});
-
+    
+    var actItem;
     for(let item in this.state.data[i].files){
       this.loadRaw(this.state.data[i].files[item].raw_url, i);
+      actItem = item;
     }
-
-    this.setState({inputCliked: true });
-    this.setState({actualIndexBox: i});
-
+    
+    this.setState({
+      actualTitleBox: actItem,
+      inputCliked: true,
+      actualIndexBox: i,
+    });
   } 
 
   handleChange(i, event) {
@@ -100,43 +115,53 @@ class Gistbox extends React.Component {
   renderPre(i) {
     return (
       <div className="code">
-        <pre>{this.state.file[i]}</pre>
+          <pre>
+            <code key={'input'+i} ref={(code) => { this.codeTag = code; }} id="rawcode" className="language-javascript">
+            {this.state.file[i]}
+            </code>
+          </pre>
       </div>
     );
   }        
 
   render() {
     var index = this.state.actualIndexBox;
+    var title = this.state.actualTitleBox;
+
+    //---OJO VER CUANDO SE RENDERIZA Y PORQUE?
+    console.log('render!!');
+
     return (
-        <div>
+        <div className="wrapper">
           <div className='view'>
             {
               this.state.inputCliked?
     
-              <div className='inner'>
-                <div>{this.state.data[index].description}</div>
-                <div>{this.state.data[index].description}</div>
+              <div className="inner">
+                <div className="info">
+                  <div className="title">{title}</div>
+                  <div>{this.state.data[index].description}</div>
+                </div>
                 {this.renderPre(index)}
              </div>
 
               :
     
-              <div>EMPTY</div>
+              <div className="inner">EMPTY</div>
             }
     
           </div>
-          <ul className='grid'>
+          <div className='grid'>
             { this.state.data.map((item, i) => {
-              return <li key={item.id} className='item'>
-                <div className='inner'>
+              return <div key={item.id} className={`item ${i === index ? 'item_active' : 'item_inactive'}`}>
+                <div className="item__inner">
                   <div>{item.description}</div>
-                  {this.renderInput(i)}
                   {this.renderButton(i)}
                </div>
-              </li>
+              </div>
               })
             }
-          </ul>
+          </div>
         </div>
     );
   }
@@ -147,7 +172,19 @@ class Gistbox extends React.Component {
       
 ReactDOM.render(<Gistbox elquery="go"/>, document.getElementById('app'));
 
+/*
 
+UX todo:
+- marcar el snipet actual
+- mostrar el ultimo by default
+- BTN VIEW RAW para copiar!
+
+CODE:
+
+- mejorar los componentes, individualizarlos
+- limpiar y unificar codigo
+
+*/
 
 
 
